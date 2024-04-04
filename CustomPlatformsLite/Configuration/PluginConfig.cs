ef35d0acbc936d81aae2089055be1d7bbcdf82e5
qqrz997 <156676353+qqrz997@users.noreply.github.com>
2024-04-04 22:41:33 +0100

    Add project files.

---
+++ CustomPlatformsLite/Configuration/PluginConfig.cs
@@ -0,0 +1,28 @@
+﻿using System.Runtime.CompilerServices;
+using IPA.Config.Stores;
+
+[assembly: InternalsVisibleTo(GeneratedStore.AssemblyVisibilityTarget)]
+namespace CustomPlatformsLite.Configuration
+{
+    internal class PluginConfig
+    {
+        public static PluginConfig Instance { get; set; }
+
+        public virtual int IntValue { get; set; } = 42; 
+
+        public virtual void OnReload()
+        {
+
+        }
+
+        public virtual void Changed()
+        {
+            // Do stuff when the config is changed.
+        }
+
+        public virtual void CopyFrom(PluginConfig other)
+        {
+
+        }
+    }
+}
